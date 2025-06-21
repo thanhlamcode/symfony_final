f27318a15a35561d44563e00dbbf8944bc7c7314
thanhlamcode <lamdoan1122334455@gmail.com>
2025-06-21 22:50:58 +0700

    define entity

--- src/Entity/Order.php
+++ src/Entity/Order.php
@@ -3,7 +3,6 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
-use App\Repository\OrderRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;

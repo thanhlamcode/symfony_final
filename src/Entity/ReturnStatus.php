6850470dfab2ce472f897ac8062303dab15f18ab
thanhlamcode <lamdoan1122334455@gmail.com>
2025-06-21 16:41:51 +0000

    fix bug

--- src/Entity/ReturnStatus.php
+++ src/Entity/ReturnStatus.php
@@ -1,1 +1,12 @@
- 
+<?php
+
+namespace App\Entity;
+
+enum ReturnStatus: string
+{
+    case REQUESTED = 'requested';
+    case APPROVED = 'approved';
+    case REJECTED = 'rejected';
+    case RETURNED = 'returned';
+    case REFUNDED = 'refunded';
+}
